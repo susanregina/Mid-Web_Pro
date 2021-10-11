acfdd50a6143e3114be94c456427f1607ddb7a1d
Joseph Susan Regina <69851068+susanregina@users.noreply.github.com>
2021-10-11 08:46:40 +0800

    Add files via upload

---
+++ MID_WEBPRO/ComboboFix.js
@@ -0,0 +1,6 @@
+// Add Student Combobox hotfix
+$("#sel1").change(function () {
+    var id = $(this).val();
+	var options = $(this).data('options').filter('[class=' + id + ']' || '[class="0"]');
+	$('#sel2').html(options);
+}); 
---
+++ MID_WEBPRO/DefArray.js
@@ -0,0 +1,27 @@
+//default array
+var studentsData = [
+	{
+		id: 1,
+		NIM: 174588746664,
+		fullName: '	Reva Mamonto',
+		gender: 'female',
+		fakultas: 'FIK',
+		study: 'Sistem Informasi',
+	},
+	{
+		id: 2,
+		NIM: 5458887488964,
+		fullName: 'Michelle Tololiu',
+		gender: 'female',
+		fakultas: 'FEB',
+		study: 'Akuntansi',
+	},
+	{
+		id: 3,
+		NIM: 78874656449,
+		fullName: 'Gloria Moningka',
+		gender: 'Female',
+		fakultas: 'FEB',
+		study: 'Manajemen',
+	}
+]
